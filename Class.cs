4e866e53da022d1e8f82677e13c27916c329daf9
maulanarr <101683721+maulanarr@users.noreply.github.com>
2022-05-18 00:31:08 +0700

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utsmaulana
+{
+    internal class Dosen
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private int nik { get; set; }
+        private string gender { get; set; }
+        private string course { get; set; }
+
+        public Dosen(int id, string name, int nik, string gander, string course)
+        {
+            this.id = id;
+            this.name = name;
+            this.nik = nik;
+            this.gender = gander;
+            this.course = course;
+        }
+        public void cetakHasil()
+        {
+            Console.WriteLine("Data Default Dosen");
+            Console.WriteLine();
+            Console.WriteLine($"ID : {this.id}");
+            Console.WriteLine($"Name : {this.name}");
+            Console.WriteLine($"NIK : {this.nik}");
+            Console.WriteLine($"Gender : {this.gender}");
+            Console.WriteLine($"Course : {this.course}");
+        }
+
+        public void addDosen()
+        {
+            Console.Write("Inputkan Nama Dosen Terbaru : ");
+            this.name = Console.ReadLine();
+            Console.Write("Inputkan NIK Dosen Terbaru : ");
+            this.nik = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public string addCourse()
+        {
+            Console.Write($"Inputkan Course {this.course} : ");
+            this.course = Console.ReadLine();
+
+            return this.name;
+        }
+
+        public void cetakAkhir()
+        {
+            Console.WriteLine("Data Dosen Terbaru");
+            Console.WriteLine();
+            Console.WriteLine($"Name : {name}");
+            Console.WriteLine($"NIK : {nik}");
+            Console.WriteLine($"Course : {course}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utsmaulana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen(1, "Maulana Rizky Rahmadhani", 21114465, "Laki-laki", "Pemrograman");
+
+            dosen1.cetakHasil();
+            Console.WriteLine();
+            dosen1.addDosen();
+            dosen1.addCourse();
+            Console.WriteLine();
+            dosen1.cetakAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
+
